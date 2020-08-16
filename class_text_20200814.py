e3b55f920c76e72299bb9e8a4b2d468b44d1f026
SongM <ucemso0@vt.edu>
2020-08-16 18:02:00 -0400

    Add files via upload

---
+++ class_text_20200814.py
@@ -0,0 +1,51 @@
+class text:
+    def __init__(self):
+        self.file_add = ""
+        self.s = ""
+        self.m = {}
+    def getTextFromFile(self,file_add):
+        f = open(file_add, encoding="utf8")
+        self.file_add = file_add
+        self.s = f.read()
+    def splitText(self):
+        punctuation_list = ",.!?:; \"“”-_\'(){}[]\n#1234567890$+-*/—%’‘"
+        s = self.s
+        for p in punctuation_list:
+            s=s.replace(p,",")
+        word_list = s.split(",")
+        for word in word_list:
+            if (word==""):
+                continue
+            word = word.lower()
+            if word in self.m:
+                self.m[word] += 1
+            else:
+                self.m[word] = 1
+    def printWordList_wordList(self,reverse=False):
+        pairs_keys = []
+        for key in self.m:
+            pairs_keys.append([key, self.m[key]])
+        pairs_keys.sort(reverse = reverse)
+        print(pairs_keys)
+    def printWordList_Rank(self,reverse=False):
+        pairs_values = []
+        for key in self.m:
+            pairs_values.append([self.m[key], key])
+        pairs_values.sort(reverse = not reverse)
+        print(pairs_values)
+    def save(self, save_add):
+    def load(self, load_add, b_load_str=False):
+
+
+folder_name = r"C:\Users\s\Google Drive\s\dropbox_extention\helprobot_20200813\help_vocabulary_20200813\text_data\pride_and_prejudice"
+# file_name = "chapter_1.txt"
+file_name = "full_text.txt"
+
+file_add = folder_name + "\\" + file_name
+t = text()
+t.getTextFromFile(file_add)
+t.splitText()
+t.printWordList_wordList()
+t.printWordList_wordList(reverse=True)
+t.printWordList_Rank()
+t.printWordList_Rank(reverse=True)
---
+++ main_20200813.py
@@ -0,0 +1,35 @@
+class text{
+    def getTextFromFile(file_add):
+    # takes too much space
+    # self.text_str
+    def splitText()->List[word]:
+    #     s = self.text_str
+
+}
+
+class wordList{
+    self.word_list
+    self.word_outOfDictionary
+    self.dictionaryVersion
+    def ReadArticle(str s)->List[word]:
+    def compareTwoWordList(wordlist1:wordList,wordlist2:wordList)
+    def combineTwoWordList(wordlist1:wordList,wordlist2:wordList)
+
+
+
+}
+
+class word{
+    self.word_str
+    self.count
+    self.mastery_degree
+    self.word_variation_list
+}
+
+class wholeDictionary{
+    self.dictionaryVersion
+    def isWord(str)->bool:
+}
+
+t = text()
+t.getTextFromFile(file_add)
